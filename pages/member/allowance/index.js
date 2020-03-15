00d7e0d602a351ee04dea6528e3f3e16f2de0fbc
1334213879 <1334213879@qq.com>
2020-03-15 10:51:05 +0800

    抵用金

---
+++ pages/member/allowance/index.js
@@ -0,0 +1,68 @@
+var e = getApp(),
+  t = e.requirejs("core"),
+  a = e.requirejs("jquery");
+
+Page({
+  data: {
+    disabled: !0,
+    coupon: {
+      count: 0
+    },
+    money: 0,
+    sindex: '',
+    nickname: '',
+    openid: ''
+  },
+  onLoad: function(t) {
+    e.url(t), this.get_list();
+  },
+  get_list: function() {
+    var e = this;
+    t.get("member/allowance", {}, function(t) {
+      console.log(t)
+      e.setData(t);
+    });
+  },
+  bindKeyInput: function(e) {
+    var index = e.detail.value;
+    var openid = this.data.list[index];
+    this.setData({
+      nickname: openid.nickname,
+      openid: openid.openid,
+    })
+  },
+  money: function(e) {
+    var money = e.detail.value;
+    this.setData({
+      money: money,
+    })
+  },
+  submit: function() {
+    var openids = this.data.openid;
+    var money = this.data.money;
+    var credit = this.data.credit;
+    wx.showModal({
+      title: '提示',
+      content: '确认转发抵用金',
+      success(res) {
+        if (res.confirm) {
+          if (openids == '' || !money) {
+            return false;
+          }
+          t.get("member/allowance/submit", {
+            openids: openids,
+            money: money,
+            credit: credit
+          }, function(t) {
+            wx.showToast({
+              title: t.result.message,
+              duration: 2000
+            })
+          });
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  }
+});
